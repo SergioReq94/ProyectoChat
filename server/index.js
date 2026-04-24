468484deb1108f1ecde8990ed7cb977e6ce0235e
SergioReq94 <1851937@alu.murciaeduca.es>
2026-04-24 20:12:44 +0200

    websocket creado

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import logger from 'morgan';
 
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app); // Create an HTTP server
+const io = new Server(server); // Attach Socket.IO to the HTTP server
+
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
     res.sendFile('./client/index.html', { root: '.' });
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+});
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
